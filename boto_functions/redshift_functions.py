5923206420fdc11d831406bb9771d6c4a5e977ca
avsngh-git <avinjsingh@gmail.com>
2022-08-09 11:33:21 +0530

    added redshift functions

---
+++ boto_functions/redshift_functions.py
@@ -0,0 +1,28 @@
+from botocore.exceptions import ClientError
+import json
+from logger_project import logger
+
+def create_redshift_cluster(client, cluster_identifier, cluster_type, node_type, master_username, master_password, iam_role):
+    """Creates an AWS Redshift Cluster"""
+
+    try:
+        cluster = client.create_cluster(
+            ClusterIdentifier=cluster_identifier,
+            ClusterType=cluster_type,
+            NodeType=node_type,
+            MasterUsername=master_username,
+            MasterUserPassword=master_password,  
+            AutomatedSnapshotRetentionPeriod=0,
+            ManualSnapshotRetentionPeriod=1,
+            Port=5739,          
+            PubliclyAccessible=True,
+            DefaultIamRoleArn=iam_role,
+        )
+        logger.info(f'redshift cluster {cluster_identifier} was created')
+    
+    except ClientError:
+        logger.info(f'failed to create redshift cluster {cluster_identifier}')
+        raise
+    else:
+        return cluster
+    
